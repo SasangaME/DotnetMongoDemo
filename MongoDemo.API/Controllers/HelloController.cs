6972ce14c80a7d97f533d652fded811ef086d2cf
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2024-11-10 21:38:55 +0530

    initial commit

---
+++ MongoDemo.API/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDemo.API.Database;
+
+namespace MongoDemo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController(MongoContext mongoContext) : ControllerBase
+    {
+        // GET: api/<HelloController>
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var items = await mongoContext.Items.Find(item => true).ToListAsync();
+            if (!items.Any())
+            {
+                var item = new Item
+                {
+                    Name = "demo item",
+                    Description = "demo description"
+                };
+                await mongoContext.Items.InsertOneAsync(item);
+            }
+            return await Task.FromResult("Hello World!");
+        }
+    }
+}
---
+++ MongoDemo.API/Database/MongoContext.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace MongoDemo.API.Database;
+
+public class MongoContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("MongoConnection"));
+        _database = client.GetDatabase("cqrs");
+    }
+
+    public IMongoCollection<Item> Items => _database.GetCollection<Item>("demo");
+
+}
+
+public class Item
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
